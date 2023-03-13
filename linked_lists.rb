b80eeb8a230d819110c27a98510d27ae6d7460fb
Sergio Guerra <sergioguerra6188@gmail.com>
2023-03-13 12:57:57 -0400

    Add Node and LinkedList classes with empty methods
    Methods for classes above have been added but have not been implemented.

---
+++ linked_lists.rb
@@ -0,0 +1,36 @@
+class LinkedList
+  def append(value)
+  end
+
+  def prepend(value)
+  end
+
+  def size
+  end
+
+  def head
+  end
+  
+  def tail
+  end
+
+  def at(index)
+  end
+
+  def contains?(value)
+  end
+
+  def find(value)
+  end
+
+  def to_s
+  end
+end
+
+class Node
+  def value
+  end
+
+  def next_node
+  end
+end
