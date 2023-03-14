0e6a9ba922e36ffb87d1110201fe661e3d9306ac
Sergio Guerra <sergioguerra6188@gmail.com>
2023-03-13 22:44:44 -0400

    Add initialize method to LinkedList
    This initialize method will initialize the list with a nil node. It can
    be initialized with a value but it does not have to be.
    
    Also allowed nodes to be initialized with values. Default is nil so that
    it can be assigned later.

--- linked_lists.rb
+++ linked_lists.rb
@@ -1,4 +1,8 @@
 class LinkedList
+  def initialize(value=nil)
+    head_node = Node.new(value)
+  end
+
   def append(value)
   end
 
@@ -30,9 +34,9 @@ end
 class Node
   attr_accessor :value
 
-  def initialize
-    @value = nil
-    @next_node = nil
+  def initialize(value=nil, next_node=nil)
+    @value = value
+    @next_node = next_node
   end
 
   def next_node
